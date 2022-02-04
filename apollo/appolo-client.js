23e03ecd8ab99245ea811ebda2f23805b7367e95
Borys Lahno <51653827+boryslahno@users.noreply.github.com>
2022-02-04 17:54:00 +0200

    Update appolo-client.js

--- apollo/appolo-client.js
+++ apollo/appolo-client.js
@@ -4,7 +4,7 @@ const client = new ApolloClient({
    uri: 'https://api.github.com/graphql',
    cache: new InMemoryCache(),
    headers: {
-      authorization: `Bearer ${process.env.TOKEN}`
+      authorization: `Bearer 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`
    }
 })
 
