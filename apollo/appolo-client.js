8889b037dad3e883f0fba96fe34226489890dec0
boryslahno <borys.lahno@gmail.com>
2022-02-07 12:54:40 +0200

    Changed personal access toke

--- apollo/appolo-client.js
+++ apollo/appolo-client.js
@@ -4,7 +4,7 @@ const client = new ApolloClient({
    uri: 'https://api.github.com/graphql',
    cache: new InMemoryCache(),
    headers: {
-      authorization: `Bearer ${'ghp_xo/IiA/ER3b/Mq8Ao/GdZ0eS/GVLT2e/l7RP0/oN7gf'.replace(/\//g, '')}`
+      authorization: 'Bearer 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'
    }
 })
 
